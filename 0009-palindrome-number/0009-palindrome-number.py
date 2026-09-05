c1d7b3a651056367583b82f492ecbde44ab0b92e
Selva Ganapathy V P <selvaganapathyvp05@gmail.com>
2026-09-05 15:25:35 +0530

    Time: 16 ms (30.63%), Space: 12.1 MB (99.44%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x<0:
+            return False
+        n = x
+        c = 0
+        while n!=0:
+            c=c*10 + n%10
+            n//=10
+        if x==c:
+            return True
+        return False
