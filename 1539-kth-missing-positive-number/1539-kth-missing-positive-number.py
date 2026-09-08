e0af29afe0bb77365d1a2cc7661d7fae01d61507
Selva Ganapathy V P <selvaganapathyvp05@gmail.com>
2026-09-08 20:09:51 +0530

    Time: 111 ms (14.23%), Space: 12.4 MB (55.07%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def findKthPositive(self, arr, k):
-        arr_set = set(arr)
         for i in range(1, k + len(arr) + 1):
-            if i not in arr_set: 
+            if i not in arr: 
                 k -= 1
             if k == 0:
                 return i
